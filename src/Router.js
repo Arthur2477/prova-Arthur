8f37decc3d7ec4bc7b1344ccb6168ef4b759fefa
Pedrinho Dev <103223807+QAP2K22@users.noreply.github.com>
2023-09-26 21:53:36 -0300

    Add files via upload

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,19 @@
 import React from 'react'
-import {NavigatorConteiner} from '@react-navigation/native'
+import Home from '../Screens/Home';
 
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Home from '../Screens/Home';
 
 const Stack = createNativeStackNavigator()
 
 export default function Router() {
   return (
-    
-        <NavigatorConteiner>
-        <Stack.Navigator initalRouterName= 'Home'>
-        <Stack.Screen name= 'Home' component={Home}/>
-        </Stack.Navigator>
-        </NavigatorConteiner>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
